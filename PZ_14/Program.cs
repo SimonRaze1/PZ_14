2fc8cfa5fb58fcc842b53fabf38de2c35de3c32f
User <User@DESKTOP-AMGP8UK>
2023-12-02 21:53:52 +0500

    Добавьте файлы проекта.

---
+++ PZ_14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        using (StreamReader file1 = new StreamReader("f1.txt"), file2 = new StreamReader("f2.txt"))
+        {
+            int lineNum = 0;
+            string line1, line2;
+            while ((line1 = file1.ReadLine()) != null && (line2 = file2.ReadLine()) != null)
+            {
+                lineNum++;
+                if (line1 != line2)
+                {
+                    Console.WriteLine($"Расхождение обнаружено в строке {lineNum}:");
+                    Console.WriteLine($"Файл 1: {line1}");
+                    Console.WriteLine($"Файл 2: {line2}");
+                }
+            }
+        }
+    }
+}
